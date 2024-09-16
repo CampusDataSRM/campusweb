e02f5b84f5c13854a3fbb9980e418253366c2530
vaibhavraj-exe <vraj.exe@gmail.com>
2024-09-17 01:13:23 +0530

    chore: install button to right

--- components/global/InstallButton.js
+++ components/global/InstallButton.js
@@ -37,7 +37,7 @@ const InstallButton = () => {
       {isInstallable && (
         <button
           onClick={handleInstallClick}
-          className="absolute left-7 bottom-7 px-4 py-4 bg-gradient-to-br from-theme_primary to-theme_secondary text-white rounded-full hover:bg-blue-700"
+          className="absolute right-7 bottom-7 px-4 py-4 bg-gradient-to-br from-theme_primary to-theme_secondary text-white rounded-full hover:bg-blue-700"
         >
           <svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
