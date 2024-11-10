f7242091006a16f8597ec4890e3903a48023a0a9
Aditya-Coomar <adi.coomar04@gmail.com>
2024-11-10 19:06:56 +0530

    chore: deployment

--- components/student/timetable/dashboard/timetable-card.jsx
+++ components/student/timetable/dashboard/timetable-card.jsx
@@ -8,7 +8,9 @@ const DasbboardTimetableCard = ({
     <>
       <div
         className={`${
-          subjectName.includes("No class") ? "bg-[#0C4DA2]/20" : "bg-theme_primary/95"
+          subjectName.includes("No class")
+            ? "bg-[#0C4DA2]/20"
+            : "bg-theme_primary/95"
         } px-2 py-[10px] rounded-lg text-sm tracking-wider`}
       >
         <div className="w-full flex justify-start gap-3 items-center">
