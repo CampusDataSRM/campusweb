62b698b331cd7773deb0cf8b89cdeddbb12e09ad
Aditya-Coomar <adi.coomar04@gmail.com>
2025-02-22 03:50:32 +0530

    chore: disable download timetable

--- app/student/timetable/page.jsx
+++ app/student/timetable/page.jsx
@@ -122,9 +122,10 @@ const Timetable = () => {
         <main className="px-4">
           <div className="flex justify-between items-center">
             <SectionTitle title="Timetable" />
-            <button
+            {/*<button
               className="z-10 bg-gradient-to-br from-theme_primary/90 to-theme_secondary/90 p-2 rounded-md text-theme_text_normal text-center tracking-wider text-xs font-semibold flex items-center justify-center gap-2"
               onClick={() => downloadImage("timetable")}
+              disabled
             >
               <span>Download</span>
               <svg
@@ -136,7 +137,7 @@ const Timetable = () => {
               >
                 <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" />
               </svg>
-            </button>
+            </button>*/}
           </div>
           {loading ? (
             <div className="flex justify-center mt-60">
