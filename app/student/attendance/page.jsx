02cf83d9f98deb7d2a0d8f0eae87697b5616891d
Vaibhav Raj <vraj.exe@gmail.com>
2025-09-11 10:43:13 +0530

    chore: remove sponsor from attendance page

--- app/student/attendance/page.jsx
+++ app/student/attendance/page.jsx
@@ -290,14 +290,14 @@ const Attendance = () => {
             </div>
           )}
           {/* Sponsor Advertisement */}
-          <Link href="https://play.google.com/store/apps/details?id=com.printellect.printellect">
+          {/* <Link href="https://play.google.com/store/apps/details?id=com.printellect.printellect">
           <div className="font-black py-4 px-4 mb-5 rounded-lg text-start md:text-center bg-[#044aaa]">
             <p className="text-theme_text_normal font-medium tracking-wide">
               Lift line? Unavoidable. Print line? Optional. <br />
               <span className="text-theme_primary hover:underline">Tap here</span> to skip the one you can. #PRINTELLECT
             </p>
           </div>
-          </Link>
+          </Link> */}
           {loading ? (
             <div className="flex justify-center mt-60 content-center">
               <Loader />
