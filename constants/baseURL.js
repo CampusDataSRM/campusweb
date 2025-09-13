5cf9652fd9b606cb6f4a43795112f81c3707e662
Vaibhav Raj <vraj.exe@gmail.com>
2025-09-13 13:47:02 +0530

    feat: randomize API

--- constants/baseURL.js
+++ constants/baseURL.js
@@ -1,1 +1,7 @@
-export const baseURL = process.env.NEXT_PUBLIC_SERVER;
+const servers = [
+  process.env.NEXT_PUBLIC_SERVE_1,
+  process.env.NEXT_PUBLIC_SERVE_2,
+  process.env.NEXT_PUBLIC_SERVE_3
+];
+
+export const baseURL = servers[Math.floor(Math.random() * servers.length)];
