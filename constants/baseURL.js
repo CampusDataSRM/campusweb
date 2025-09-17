ae045de4b218474f56bbd9b817ce617dedc8e8d4
Aditya Coomar <adi.coomar04@gmail.com>
2025-09-17 12:12:24 +0530

    chore: switch to server 1

--- constants/baseURL.js
+++ constants/baseURL.js
@@ -1,7 +1,7 @@
 const servers = [
   process.env.NEXT_PUBLIC_SERVE_1,
   process.env.NEXT_PUBLIC_SERVE_2,
-  process.env.NEXT_PUBLIC_SERVE_3
+  process.env.NEXT_PUBLIC_SERVE_3,
 ];
 
-export const baseURL = servers[Math.floor(Math.random() * servers.length)];
+export const baseURL = process.env.NEXT_PUBLIC_SERVE_1;
