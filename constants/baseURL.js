9d17bdb90bafcb32213559d09eab514fc11e8630
Aditya Coomar <adi.coomar04@gmail.com>
2025-10-24 00:43:02 +0530

    fix: env fetch

--- constants/baseURL.js
+++ constants/baseURL.js
@@ -11,13 +11,13 @@ const servers = [
   process.env.NEXT_PUBLIC_SERVE_9,
 ];
 
-export const baseURL = servers[0];
-export const baseURL_1 = servers[1];
-export const baseURL_2 = servers[2];
-export const baseURL_3 = servers[3];
-export const baseURL_4 = servers[4];
-export const baseURL_5 = servers[5];
-export const baseURL_6 = servers[6];
-export const baseURL_7 = servers[7];
-export const baseURL_8 = servers[8];
-export const baseURL_9 = servers[9];
+export const baseURL = process.env.NEXT_PUBLIC_SERVE;
+export const baseURL_1 = process.env.NEXT_PUBLIC_SERVE_1;
+export const baseURL_2 = process.env.NEXT_PUBLIC_SERVE_2;
+export const baseURL_3 = process.env.NEXT_PUBLIC_SERVE_3;
+export const baseURL_4 = process.env.NEXT_PUBLIC_SERVE_4;
+export const baseURL_5 = process.env.NEXT_PUBLIC_SERVE_5;
+export const baseURL_6 = process.env.NEXT_PUBLIC_SERVE_6;
+export const baseURL_7 = process.env.NEXT_PUBLIC_SERVE_7;
+export const baseURL_8 = process.env.NEXT_PUBLIC_SERVE_8;
+export const baseURL_9 = process.env.NEXT_PUBLIC_SERVE_9;
