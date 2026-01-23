f15e378555350b2cd10eefc6ed71dfa0d4a6d763
Aditya Coomar <adi.coomar04@gmail.com>
2026-01-24 01:29:45 +0530

    fix: ux

--- components/global/andriodDeviceCheck.js
+++ components/global/andriodDeviceCheck.js
@@ -6,6 +6,7 @@ import Link from "next/link";
 
 const AndroidDeviceCheck = () => {
   const [isAndroidDevice, setIsAndroidDevice] = useState(false);
+  const [bannerDismissed, setBannerDismissed] = useState(false);
   useEffect(() => {
     setIsAndroidDevice(isAndroid());
   }, []);
@@ -13,15 +14,15 @@ const AndroidDeviceCheck = () => {
   return (
     <>
       {" "}
-      {isAndroidDevice &&
-        !sessionStorage.getItem("androidAppBannerDismissed") && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      {isAndroidDevice && !bannerDismissed && !sessionStorage.getItem("androidAppBannerDismissed") && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
             <div className=" backdrop-blur-xl bg-theme_primary/10 rounded-lg shadow-lg w-11/12 md:w-1/2">
               <button
                 className="absolute top-3 right-3 text-white text-2xl font-extrabold px-3 py-1 rounded-full bg-red-700/60 transition duration-300"
-                onClick={() =>
-                  sessionStorage.setItem("androidAppBannerDismissed", true)
-                }
+                onClick={() => {
+                  sessionStorage.setItem("androidAppBannerDismissed", true);
+                  setBannerDismissed(true);
+                }}
               >
                 &times;
               </button>
